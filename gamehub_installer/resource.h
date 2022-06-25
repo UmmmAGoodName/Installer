2d632b76b59d973460639d5c8ec55ed026a4de63
UmmmAGoodName <Philiprouthe@gmail.com>
2022-06-25 16:46:54 +0200

    first

---
+++ gamehub_installer/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by gamehub_installer.rc
+//
+#define IDC_MYICON                      2
+#define IDD_GAMEHUBINSTALLER_DIALOG     102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_GAMEHUBINSTALLER            107
+#define IDI_SMALL                       108
+#define IDC_GAMEHUBINSTALLER            109
+#define IDR_MAINFRAME                   128
+#define IDC_MFCBUTTON1                  1002
+#define ID_FILE_WEBSITE                 32771
+#define ID_HELP_GITHUB                  32772
+#define ID_HELP_WIKI                    32773
+#define ID_GITHUB_CLIENT                32774
+#define ID_GITHUB_WEB                   32775
+#define ID_GITHUB_INSTALLER             32776
+#define ID_GITHUB_API                   32777
+#define ID_WIKI_CLIENT                  32778
+#define ID_WIKI_WEB                     32779
+#define ID_WIKI_INSTALLER               32780
+#define ID_WIKI_API                     32781
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32782
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
